27dd219604100f05a9a473b733408a4040d28f9a
红鼻子猫 <165950444+wg-ocean@users.noreply.github.com>
2024-04-08 12:03:58 +0800

    Update TestController.java

--- src/main/java/com/cy/controller/TestController.java
+++ src/main/java/com/cy/controller/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
 	
 	@RequestMapping("/test")
 	public String test() {
-    return "test----success-aaaaaaaaaaaaaaaaa---bbbbbbbbbbbbbbbbb-------uuuuuuuu";
+    return "test----success-aaaaaaaaaaaaaaaaa---bbbbbbbbbbbbbbbbb-------uuuuuuuu-----yyyggg";
   }
 	
 }
